3a5c13db3481bbf9652d6332a3abd6ddb57fdf9e
juanchycc <64942535+juanchycc@users.noreply.github.com>
2021-06-24 19:26:53 -0300

    Add files via upload

--- Tp2.py
+++ Tp2.py
@@ -1,15 +1,17 @@
+from os import X_OK
 import numpy as np
 
-def leer_archivo():
+#funciones auxiliares
+def leer_archivo(nombre_archivo):
 	cantidad_elementos = 0
 	temperatura = []
 	variacion_iodo = []
 
-	with open("data_00.txt","r") as file:                         
+	with open(nombre_archivo,"r") as file:                         
 		for linea in file.readlines():      
 			linea = linea.split()
 			temperatura.append(float(linea[0]))
-			variacion_iodo.append(float(linea[1]) / 1000 ) #cambio de unidad
+			variacion_iodo.append(float(linea[1])) #cambio de unidad (lo saque porque fallecia incluso antes)
 			cantidad_elementos += 1
 		
 
@@ -24,13 +26,13 @@ def inicializar_matriz(filas,columnas):
 			matriz[i].append(0)
 	return matriz
 
-#por ahora no la use era mejor hacer todos los prod vactoriales en la misma iteracion
-def producto_vectorial(primer_vector,segundo_vector,cantidad_elementos):
-	aux = 0
-	for i in range(cantidad_elementos):
-		aux += (primer_vector[i]*segundo_vector[i])
-	return aux
+def printear_matriz(matriz): # es solo para printear ordenadamente
+	for i in range(len(matriz)):
+		for j in range(len(matriz[0])):
+			print(matriz[i][j],end=" ")
+		print()
 
+#item a
 def ajuste_lineal(temperatura,variacion_iodo,cantidad_elementos):
 	fi_cero = []
 	fi_uno = temperatura
@@ -124,7 +126,7 @@ def ajuste_polinomial(temperatura,variacion_iodo,cantidad_elementos):
 	print(error)	
 	return error
 
-#linealiza como ln( 1/SI - 1 ) = ( ln(a) . 1 ) - (b . T) --> no funciona, por el logaritmo con negativo
+#linealiza como ln( 1/SI - 1 ) = ( ln(a) . 1 ) - (b . T) 
 def ajuste_logistico(temperatura,variacion_iodo,cantidad_elementos):
 	fi_cero = []
 	fi_uno = temperatura
@@ -163,6 +165,62 @@ def ajuste_logistico(temperatura,variacion_iodo,cantidad_elementos):
 	print(error)
 	return error
 
+#item b
+def ajuste_cuadratico_sin_linealizar(temperatura,variacion_iodo,cantidad_elementos,tolerancia):
+	solucion2 = np.matrix([[190],[10],[0.05]])
+	error = 10000
+	contador = 0
+	while error > tolerancia:
+		a2 = float(solucion2[0][0])		
+		b2 = float(solucion2[1][0])
+		c2 = float(solucion2[2][0])
+		
+		valores_funcion2 = funcion_sigmoidea(a2,b2,c2,temperatura,variacion_iodo,cantidad_elementos)
+		jacobiano2 = obtener_jacobiano2(a2,b2,c2,temperatura,variacion_iodo,cantidad_elementos)
+		jacobiano2 = np.matrix(jacobiano2)
+		valores_funcion2 = np.matrix([[valores_funcion2[0]],[valores_funcion2[1]],[valores_funcion2[2]]])
+		delta2 = (jacobiano2**-1)*(valores_funcion2*-1)
+		vieja_solucion_2 = solucion2		
+		solucion2 = vieja_solucion_2 + delta2
+		
+		a = float(solucion2[0][0]) 
+		b = float(solucion2[1][0])
+		c = float(solucion2[2][0])
+
+		error = np.sqrt((a-a2)**2 + (b-b2)**2 + (c-c2)**2)	
+		contador += 1
+
+		print(contador)
+	return 0
+
+def funcion_sigmoidea(a,b,c,temperatura,variacion_iodo,cantidad_elementos):
+	valores_funcion = [0,0,0]
+	for i in range(cantidad_elementos):
+		x = temperatura[i]
+		y = variacion_iodo[i]
+		valores_funcion[0] += 2*(y- a*np.e**(-b*np.e**(-c*x))) *  np.e**(-b*np.e**(-c*x)) *(-1)
+		valores_funcion[1] += 2*(y - a*np.e**(-b*np.e**(-c*x))) *  a*np.e**(-c*x-b*np.e**(-c*x)) #*(-1) aca corregi cosas tambien
+		valores_funcion[2] += 2*(y - a*np.e**(-b*np.e**(-c*x))) *	-a*b*x*np.e**(-b*np.e**(-c*x)-c*x)  #error en esta fórmula
+	
+	return valores_funcion
+
+def obtener_jacobiano2(a,b,c,temperatura,variacion_iodo,cantidad_elementos):
+	jacobiano = [[0,0,0],[0,0,0],[0,0,0]]
+	for i in range(cantidad_elementos):
+		x = temperatura[i]
+		d = variacion_iodo[i]                
+
+		jacobiano [0][0] +=  2*np.e**(-2*b*np.e**(-c*x))  #1
+		jacobiano [0][1] += -2*(-np.e**(-np.e**(-c*x)*b-c*x)*d+2*a*np.e**(-2*np.e**(-c*x)*b-c*x))  #2
+		jacobiano [0][2] += -2*(-2*a*b*np.e**(-2*b*np.e**(-c*x)-c*x)*x+b*np.e**(-b*np.e**(-c*x)-c*x)*d*x) #3
+		jacobiano [1][0] +=  2*np.e**(-b*np.e**(-c*x)-c*x)*(d-2*np.e**(-b*np.e**(-c*x))*a)   #4
+		jacobiano [1][1] +=  2*a*(-np.e**(-np.e**(-c*x)*b-2*c*x)*d+2*a*np.e**(-2*np.e**(-c*x)*b-2*c*x))
+		jacobiano [1][2] +=  2*a*(a*np.e**(-2*b*np.e**(-c*x)-c*x)*x-2*a*b*np.e**(-2*b*np.e**(-c*x)-2*c*x)*x+b*np.e**(-b*np.e**(-c*x)-2*c*x)*d*x-np.e**(-b*np.e**(-c*x)-c*x)*d*x)
+		jacobiano [2][0] += -2*b*np.e**(-b*np.e**(-c*x)-c*x)*x*(-2*np.e**(-b*np.e**(-c*x))*a+d)
+		jacobiano [2][1] += -2*a*x*(2*a*np.e**(-2*np.e**(-c*x)*b-2*c*x)*b-np.e**(-np.e**(-c*x)*b-2*c*x)*d*b-a*np.e**(-2*np.e**(-c*x)*b-c*x)+np.e**(-np.e**(-c*x)*b-c*x)*d)		#8 nuevo
+		jacobiano [2][2] += -2*a*b*x*(a*np.e**(-2*b*np.e**(-c*x)-c*x)*x-2*a*b*np.e**(-2*b*np.e**(-c*x)-2*c*x)*x+b*np.e**(-b*np.e**(-c*x)-2*c*x)*d*x-np.e**(-b*np.e**(-c*x)-c*x)*d*x)  #9 nuevo
+
+	return jacobiano
 
 #item c
 def ajuste_polinomial_tres(temperatura,variacion_iodo,cantidad_elementos):
@@ -172,66 +230,111 @@ def ajuste_polinomial_tres(temperatura,variacion_iodo,cantidad_elementos):
 	fi_tres = []
 	H = variacion_iodo
 	filas = 4
-	columnas = 5
+	columnas = 4
 
 	for i in range(cantidad_elementos):
 		fi_cero.append(1)
 		fi_dos.append( ( fi_uno[i] * fi_uno[i] ) )
 		fi_tres.append( ( fi_dos[i] * fi_uno[i] ) )
 
-	matriz = inicializar_matriz(filas,columnas)
+	A = inicializar_matriz(filas,columnas)
 
 	for i in range(cantidad_elementos):
-		matriz[0][0] += ( fi_cero[i] * fi_cero[i] )
-		matriz[0][1] += ( fi_cero[i] * fi_uno[i] )
-		matriz[0][2] += ( fi_cero[i] * fi_dos[i] )
-		matriz[0][3] += ( fi_cero[i] * fi_tres[i] )
-		matriz[0][4] += ( fi_cero[i] * H[i] )
+		A[0][0] += ( fi_cero[i] * fi_cero[i] )
+		A[0][1] += ( fi_cero[i] * fi_uno[i] )
+		A[0][2] += ( fi_cero[i] * fi_dos[i] )
+		A[0][3] += ( fi_cero[i] * fi_tres[i] )
 
-		matriz[1][0] =	matriz[0][1]
-		matriz[1][1] += ( fi_uno[i] * fi_uno[i] )
-		matriz[1][2] += ( fi_uno[i] * fi_dos[i] )
-		matriz[1][3] += ( fi_uno[i] * fi_tres[i] ) 
-		matriz[1][4] += ( fi_uno[i] * H[i] )
+		A[1][0] =	A[0][1]
+		A[1][1] += ( fi_uno[i] * fi_uno[i] )
+		A[1][2] += ( fi_uno[i] * fi_dos[i] )
+		A[1][3] += ( fi_uno[i] * fi_tres[i] ) 
 
-		matriz[2][0] = matriz[0][2]
-		matriz[2][1] = matriz[1][2]
-		matriz[2][2] += ( fi_dos[i] * fi_dos[i] )
-		matriz[2][3] += ( fi_dos[i] * fi_tres[i] )
-		matriz[2][4] += ( fi_dos[i] * H[i] )
+		A[2][0] = A[0][2]
+		A[2][1] = A[1][2]
+		A[2][2] += ( fi_dos[i] * fi_dos[i] )
+		A[2][3] += ( fi_dos[i] * fi_tres[i] )
 
-		matriz[3][0] = matriz[0][3]
-		matriz[3][1] = matriz[1][3]
-		matriz[3][2] = matriz[2][3]
-		matriz[3][3] += ( fi_tres[i] * fi_tres[i] )
-		matriz[3][4] += ( fi_tres[i] * H[i] )
+		A[3][0] = A[0][3]
+		A[3][1] = A[1][3]
+		A[3][2] = A[2][3]
+		A[3][3] += ( fi_tres[i] * fi_tres[i] )
 
-	#método de doolitle (descomposición LU)
-	#triangulación
-	m21 = ( matriz[1][0] / matriz[0][0] )
-	m31 = ( matriz[2][0] / matriz[0][0] )
-	m41 = ( matriz[3][0] / matriz[0][0] )
+	aux1 = inicializar_matriz(filas,columnas)
+	aux2 = inicializar_matriz(filas,columnas)
+	for i in range(filas):
+		for j in range(columnas):
+			aux1[i][j] = A[i][j]
+			aux2[i][j] = A[i][j]
 
-	for i in range(columnas):
-		matriz[1][i] = ( ( matriz[0][i] * m21 ) - matriz[1][i] )
-		matriz[2][i] = ( ( matriz[0][i] * m31 ) - matriz[2][i] )
-		matriz[3][i] = ( ( matriz[0][i] * m41 ) - matriz[3][i] )
-		
-	m32 = ( matriz[2][1] / matriz[1][1] )
-	m42 = ( matriz[3][1] / matriz[1][1] )
+	print(factorizacion_doolitle(aux1,H,3,columnas))
+	print(factorizacion_crout(A,H,3,columnas))
+	print(factorizacion_doolitle(aux2,H,6,columnas))
+	print(factorizacion_crout(A,H,6,columnas))
 
-	x = 1
-	for x in range(columnas):
-		matriz[2][x] = ( ( matriz[1][x] * m32 ) - matriz[2][x] )
-		matriz[3][x] = ( ( matriz[1][x] * m42 ) - matriz[3][x] )
-
-	m43 = ( matriz[3][2] / matriz[2][2])
-	x = 2
-	for x in range(columnas):
-		matriz[3][x] = ( ( matriz[2][x] * m43 ) - matriz[3][x] )
+def factorizacion_crout(A,B,cantidad_digitos,columna):
+	#resuelvo las ecuaciones para obtener L y U	
+	for i in range(columna):
+		B[i] = round(B[i],cantidad_digitos)
+		for j in range(columna):
+			A[i][j] = round(A[i][j],cantidad_digitos)
 
 	L = inicializar_matriz(4,4)
-	U = matriz
+	U = inicializar_matriz(4,4)
+
+	U[0][0] = 1
+	U[1][1] = 1
+	U[2][2] = 1
+	U[3][3] = 1
+	L[0][0] = A[0][0]
+	U[0][1] = round(( A[0][1] / L[0][0] ),cantidad_digitos)
+	U[0][2] = round(( A[0][2] / L[0][0] ),cantidad_digitos)
+	U[0][3] = round(( A[0][3] / L[0][0] ),cantidad_digitos)
+	L[1][0] = A[1][0]
+	L[1][1] = round((A[1][1] - round((L[1][0] * U[0][1]),cantidad_digitos)),cantidad_digitos)
+	U[1][2] = round((round((A[1][2] - round((L[1][0] * U[0][1]),cantidad_digitos)),cantidad_digitos) / L[1][1]),cantidad_digitos)
+	U[1][3] = round(((round((A[2][1] - round((L[2][0] * U[0][1]),cantidad_digitos)),cantidad_digitos)) / L[1][0]),cantidad_digitos)
+	L[2][0] = A[2][0]
+	L[2][1] = round((A[2][1] - round((L[2][0] * U[0][1]),cantidad_digitos)),cantidad_digitos)
+	L[2][2] = round((A[2][2] - round((L[2][0] * U[0][1]),cantidad_digitos) - round((L[2][1] * U[1][2]),cantidad_digitos)),cantidad_digitos)
+	U[2][3] = round(((round((A[2][2] - round((L[2][0] * U[0][3]),cantidad_digitos) - round((L[2][1] * U[1][3]),cantidad_digitos)),cantidad_digitos)) / L[2][2]),cantidad_digitos)
+	L[3][0] = A[3][0]
+	L[3][1] = round((A[3][1] - round((L[3][0] * U[0][1]),cantidad_digitos)),cantidad_digitos)
+	L[3][2] = round((A[3][2] - round((L[3][0] * U[0][2]),cantidad_digitos) - round((L[3][1] * U[1][2]),cantidad_digitos)),cantidad_digitos)
+	L[3][3] = round((A[3][3] - round((L[3][0] * U[0][3]),cantidad_digitos) - round((L[3][1] * U[1][3]),cantidad_digitos) - round((L[3][2] * U[2][3]),cantidad_digitos)),cantidad_digitos)
+
+	return resolver_sistema(L,U,B,cantidad_digitos)
+
+def factorizacion_doolitle(A,B,cantidad_digitos,columna):
+	for i in range(columna):
+		B[i] = round(B[i],cantidad_digitos)
+		for j in range(columna):
+			A[i][j] = round(A[i][j],cantidad_digitos)
+
+	m21 = round(( A[1][0] / A[0][0] ),cantidad_digitos)
+	m31 = round(( A[2][0] / A[0][0] ),cantidad_digitos)
+	m41 = round(( A[3][0] / A[0][0] ),cantidad_digitos)
+
+	for i in range(columna):
+		A[1][i] = round(( round(( A[0][i] * m21 ),cantidad_digitos) - A[1][i] ),cantidad_digitos)
+		A[2][i] = round(( round(( A[0][i] * m31 ),cantidad_digitos) - A[2][i] ),cantidad_digitos)
+		A[3][i] = round(( round(( A[0][i] * m41 ),cantidad_digitos) - A[3][i] ),cantidad_digitos)
+		
+	m32 = round(( A[2][1] / A[1][1] ),cantidad_digitos)
+	m42 = round(( A[3][1] / A[1][1] ),cantidad_digitos)
+
+	x = 1
+	for x in range(columna):
+		A[2][x] = round(( round(( A[1][x] * m32 ),cantidad_digitos) - A[2][x] ),cantidad_digitos)
+		A[3][x] = round(( round(( A[1][x] * m42 ),cantidad_digitos) - A[3][x] ),cantidad_digitos)
+
+	m43 = round(( A[3][2] / A[2][2]),cantidad_digitos)
+	x = 2
+	for x in range(columna):
+		A[3][x] = round(( round(( A[2][x] * m43 ),cantidad_digitos) - A[3][x] ),cantidad_digitos)
+
+	L = inicializar_matriz(4,4)
+	U = A
 
 	L[0][0] = 1
 	L[1][0] = m21
@@ -242,29 +345,47 @@ def ajuste_polinomial_tres(temperatura,variacion_iodo,cantidad_elementos):
 	L[3][0] = m41
 	L[3][1] = m42
 	L[3][2] = m43
-	L[4][2] = 1
+	L[3][3] = 1
+	return resolver_sistema(L,U,B,cantidad_digitos)
 
-	#  L . Y = b --> L . Y = H
-	Y = []
-	Y[0] = ( H[0] / L[0][0] )
-	Y[1] = ( ( H[1] - ( L[1][0] * Y[0] ) ) / L[1][1] )
-	Y[2] = ( ( H[2] - ( L[2][0] * Y[0]) - ( L[2][1] * Y[1] ) ) / L[2][2] )
-	Y[3] = ( ( H[3] - ( L[3][0] * Y[0]) - ( L[3][1] * Y[1]) - (L[3][2] * Y[2]) ) / L[3][3] )
-
+def resolver_sistema(L,U,B,cantidad_digitos):
+	#  L . Y = b
+	Y = [0,0,0,0]
+	Y[0] = round(( B[0] / L[0][0] ),cantidad_digitos)
+	Y[1] = round(( round(( B[1] - round(( L[1][0] * Y[0]),cantidad_digitos) ),cantidad_digitos) / L[1][1] ),cantidad_digitos)
+	Y[2] = round(( round(( B[2] - round(( L[2][0] * Y[0]),cantidad_digitos) )- round(( L[2][1] * Y[1]),cantidad_digitos) ,cantidad_digitos) / L[2][2] ),cantidad_digitos)
+	Y[3] = round(( round(( B[3] - round(( L[3][0] * Y[0]),cantidad_digitos) )- round(( L[3][1] * Y[1]),cantidad_digitos) - round((L[3][2] * Y[2]),cantidad_digitos ),cantidad_digitos) / L[3][3] ),cantidad_digitos)
 	# U . X = Y
-	X = []
-	X[3] = ( Y[3] / U[3][3] )
-	X[2] = ( ( Y[2] - ( U[2][3] * X[3] ) ) / U[2][2] )
-	X[1] = ( ( Y[1] - ( U[1][3] * X[3] ) - ( U[1][2] * X[2] ) ) / U[1][1] )
-	X[0] = ( ( Y[0] - ( U[0][3] * X[3] ) -( U[0][2] * X[2] ) - ( U[0][1] * X[1] ) ) / U[0][0])
-	
+	X = [0,0,0,0]
+	X[3] = round(( Y[3] / U[3][3] ),cantidad_digitos)
+	X[2] = round(( round(( Y[2] - round(( U[2][3] * X[3] ),cantidad_digitos) ),cantidad_digitos) / U[2][2] ),cantidad_digitos)
+	X[1] = round(( round(( Y[1] - round(( U[1][3] * X[3] ),cantidad_digitos) - round(( U[1][2] * X[2] ),cantidad_digitos) ),cantidad_digitos) / U[1][1] ),cantidad_digitos)
+	X[0] = round(( round(( Y[0] - round(( U[0][3] * X[3] ),cantidad_digitos) - round(( U[0][2] * X[2] ),cantidad_digitos) - round(( U[0][1] * X[1] ),cantidad_digitos) ),cantidad_digitos) / U[0][0]),cantidad_digitos)
+	return X
 
 def main():
-	temperatura,variacion_iodo,cantidad_elementos = leer_archivo()
 
-	ajuste_lineal(temperatura,variacion_iodo,cantidad_elementos)
-	ajuste_polinomial(temperatura,variacion_iodo,cantidad_elementos)
-	ajuste_logistico(temperatura,variacion_iodo,cantidad_elementos)
-	#ajuste_polinomial_tres(temperatura,variacion_iodo,cantidad_elementos)
+	tolerancia = 0.00000001 # (item b)
+	temperatura,variacion_iodo,cantidad_elementos = leer_archivo("data_00.txt")
+	variacion_iodo_achicada = []
+	for i in range(cantidad_elementos):
+		variacion_iodo_achicada.append( variacion_iodo[i] / 1000 )
 
+	ajuste_lineal(temperatura,variacion_iodo_achicada,cantidad_elementos)
+	ajuste_polinomial(temperatura,variacion_iodo_achicada,cantidad_elementos)
+	ajuste_logistico(temperatura,variacion_iodo_achicada,cantidad_elementos)
+	ajuste_cuadratico_sin_linealizar(temperatura,variacion_iodo,cantidad_elementos,tolerancia)
+	ajuste_polinomial_tres(temperatura,variacion_iodo_achicada,cantidad_elementos)
+
+	temperatura,variacion_iodo,cantidad_elementos = leer_archivo("data_01.txt")
+	variacion_iodo_achicada = []
+	for i in range(cantidad_elementos):
+		variacion_iodo_achicada.append( variacion_iodo[i] / 1000 )
+
+	ajuste_lineal(temperatura,variacion_iodo_achicada,cantidad_elementos)
+	ajuste_polinomial(temperatura,variacion_iodo_achicada,cantidad_elementos)
+	ajuste_logistico(temperatura,variacion_iodo_achicada,cantidad_elementos)
+	ajuste_cuadratico_sin_linealizar(temperatura,variacion_iodo,cantidad_elementos,tolerancia)
+	ajuste_polinomial_tres(temperatura,variacion_iodo_achicada,cantidad_elementos)
+	
 main()
